04a4925c89270fb5b579d5c7adddaf54c2ceb574
Yvette <yvettecook@gmail.com>
2016-04-05 07:39:30 +0100

    Initial Commit

---
+++ RxCitySearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxCitySearch
+//
+//  Created by Yvette Cook on 05/04/2016.
+//  Copyright © 2016 RxExamples. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
